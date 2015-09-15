32aa6aa0e7943ef555c333f2c9797b8d47e6e199
Gerhard <gerhard@thinkroom.co.za>
2015-09-15 11:41:59 +0200

    Foundation for development.

---
+++ Team1/Team1/Backbone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team1
+{
+    // This Class acts as the backbone of the Application. All major method calls reside here.
+    // Remeber to add comments to your code.
+    public class Backbone
+    {
+    }
+}
